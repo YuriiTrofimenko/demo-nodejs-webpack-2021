9ba899cefbfd0581a2fb46b52824f9b103921634
YuriiTrofimenko <tyaa@ukr.net>
2021-05-10 14:05:48 +0300

    dom, fetch

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,11 @@
 import * as helperModule from './my-helper-module'
 
-console.log("Welcome! Greetings from app.js. Let's learn Webpack2");
-
+console.log("Welcome! Greetings from app.js. Let's learn Webpack 4");
+const h1 = document.createElement('h1')
+h1.innerText = helperModule.greetings
+document.getElementsByTagName('my-app')[0].append(h1)
 console.log(helperModule.greetings);
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json))
